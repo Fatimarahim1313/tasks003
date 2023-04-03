691e09905be566edde7ebc4c673fb02ac6379bdf
Raxim Fatima <rahimovafatima1313@gmail.com>
2023-04-03 18:23:18 +0500

    task #3

---
+++ Task 002/Program.cs
@@ -0,0 +1,19 @@
+﻿//напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+
+int Read()
+{
+    System.Console.WriteLine("Введите число");
+    int x = int.Parse(Console.ReadLine());
+    return x;
+}
+
+void Print()
+{
+    int y = Read();
+    for (int i = 1; i <= y; i++)
+    {
+        System.Console.WriteLine(Math.Pow(i, 3));
+    }
+}
+
+Print();
---
+++ Task 003/Program.cs
@@ -0,0 +1,32 @@
+﻿//напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+
+string Read()
+{
+    System.Console.WriteLine("Введите число");
+    string txt = Console.ReadLine();
+    return txt;
+}
+
+string input = Read();
+if (input.Length != 5)
+{
+    Console.WriteLine("Введенное число не является 5 значным");
+    return;
+}
+
+string reversed = string.Empty;
+for (int i = 4; i >= 0; i--)
+{
+    reversed += input[i];
+}
+
+if (reversed == input)
+{
+    Console.WriteLine($"{input} -> да");
+}
+else
+{
+    Console.WriteLine($"{input} -> нет");
+}
+
+
